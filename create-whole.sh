cb6b3d829b24755c7ccedfd7e90b6c6a3a51fc63
kawakubo <tomo.kawakubo@gmail.com>
2023-04-05 22:01:56 +0900

    2023年04月05日授業分

--- create-whole.sh
+++ create-whole.sh
@@ -4,7 +4,7 @@ readonly BASE_DIR="$HOME/myyaml"
 readonly DEST_DIR="$HOME/myyaml_dest"
 readonly DEFAULT_TARGET="$DEST_DIR/wordpress-mysql.yaml"
 
-
+reverse_flag=1
 
 yaml_array=()
 
@@ -28,12 +28,31 @@ for path in "$BASE_DIR/*"; do
   yaml_array+=("$path")
 done
 
+echo ${#yaml_array[*]}
 
-for file_path in ${yaml_array[@]}; do
+echo ソート前
+for file_p in ${yaml_array[@]}; do
+  echo $file_p
+done
+
+#$orginal_ifs=$IFS
+#IFS=$'\n'
+if [ $reverse_flag -eq 1 ]; then
+  echo descending
+  sorted_array=($(echo ${yaml_array[@]} | sort -r))
+else
+  echo ascending
+  sorted_array=($(echo ${yaml_array[@]} | sort))
+fi
+#IFS=$orginal_ifs
+
+for file_path in ${sorted_array[@]}; do
+  echo $file_path
   echo --- >> $DEFAULT_TARGET
   while IFS= read line; do
     echo "$line" >> $DEFAULT_TARGET
   done < $file_path
 done
 
-cat $DEFAULT_TARGET | sed -i '1d' $DEFAULT_TARGET
+sed -i '1d' $DEFAULT_TARGET
+# cat $DEFAULT_TARGET | sed -i '1d' $DEFAULT_TARGET
